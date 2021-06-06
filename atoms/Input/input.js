0767949fa0137da2823e58963e373f2231760926
Camilo Mora <kalufau@hotmil.com>
2021-06-05 22:56:58 -0500

    add molecule input, configuration precommit

---
+++ atoms/Input/input.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './input.css'
+
+const Input = ({ type, value, onChange, placeholder }) => (
+  <input
+    className="input"
+    type={type}
+    value={value}
+    placeholder={placeholder}
+    onChange={onChange}
+  ></input>
+)
+
+Input.defaultProps = {
+  type: 'text',
+}
+
+Input.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf['text'],
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default Input
