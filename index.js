7583302db1d83cc218df90f7312713f1086374ac
Camilo Mora <kalufau@hotmil.com>
2021-06-08 20:40:17 -0500

    Fix export index to publish npm

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Layout
-import Spacer from '../layout/Spacer'
+import Spacer from './layout/spacer'
 
 // Atoms
-import Button from '../atoms/Button'
-import Heading from '../atoms/Heading'
-import Icon from '../atoms/Icon'
-import Input from '../atoms/Input'
-import Picture from '../atoms/Picture'
+import Button from './atoms/Button'
+import Heading from './atoms/Heading'
+import Icon from './atoms/Icon'
+import Input from './atoms/Input'
+import Picture from './atoms/Picture'
 
 // Molecules
-import ButtonIcon from '../molecules/ButtonIcon'
-import Dropdown from '../molecules/Dropdown'
+import ButtonIcon from './molecules/ButtonIcon'
+import Dropdown from './molecules/Dropdown'
 
 export { Spacer, Button, Heading, Icon, Input, Picture, ButtonIcon, Dropdown }
