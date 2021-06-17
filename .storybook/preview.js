07912532273165ffa3dc5664fcc1f7a78b04a18a
Camilo Mora <kalufau@hotmil.com>
2021-06-16 21:33:35 -0500

    Add library chromatic for visual testing components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 // FIXME: Move as asset file
-// import 'minireset.css'
-// import '../styles/globals.css'
-// import '../styles/tokens.css'
+import 'minireset.css'
+import '../styles/globals.css'
+import '../styles/tokens.css'
 
 // export const parameters = {
 //   actions: { argTypesRegex: "^on[A-Z].*" },
